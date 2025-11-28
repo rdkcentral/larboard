db683caf59bae46b6e179c4bd9b8dee409e64afd
Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>
2025-11-28 20:14:11 +0000

    Throttle audio write duration and playback position query

--- src/third_party/starboard/rdk/shared/audio_sink/gstreamer_audio_sink_type.cc
+++ src/third_party/starboard/rdk/shared/audio_sink/gstreamer_audio_sink_type.cc
@@ -276,7 +276,9 @@ GStreamerAudioSink::~GStreamerAudioSink() {
 // static
 void* GStreamerAudioSink::AudioThreadEntryPoint(void* context) {
   SB_DCHECK(context);
+#if SB_API_VERSION >= 16
   SbThreadSetPriority(kSbThreadPriorityRealTime);
+#endif
 
   GStreamerAudioSink* sink = reinterpret_cast<GStreamerAudioSink*>(context);
   GST_TRACE_OBJECT(sink->pipeline_, "TID: %d", SbThreadGetId());

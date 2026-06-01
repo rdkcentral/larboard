7a04d023bbd14f2522c9b01ca973b61eb3c1390a
Niranjan Yardi <nyardi@google.com>
2026-06-01 15:54:16 -0700

    Deprecate starboard/thread.h (#10717)
    starboard: Remove SbThreadId and thread.h
    
    - **`SbThreadId`** (Typedef): Deprecated in `starboard/thread.h`.
    Internal usages migrated to POSIX `pid_t`.
    - **`kSbThreadInvalidId`** (Macro): Removed from `starboard/thread.h`.
    Replaced with `0` in migrated code.
    - **`SbThreadIsValidId`** (Inline function): Removed from
    `starboard/thread.h`. Replaced with standard comparison (`!= 0`) in
    migrated code.
    
    - **` starboard/thread.h`** : After removing above 3 Starboard API's ,
    deprecate starboard/thread.h and remove all call sites where it is
    referenced.
    
    Issue: 457772688
    Issue: 517657866
    
    Origin: https://github.com/youtube/cobalt/commit/d9f3e6e2685954093b9b4466ca3a60685b92f958 (main)
    Cherry-Pick: https://github.com/youtube/cobalt/commit/f36adf2c437785adee76f542b5e3d5e5fe823fcf (27.lts)

--- src/third_party/starboard/rdk/shared/player/player_internal.cc
+++ src/third_party/starboard/rdk/shared/player/player_internal.cc
@@ -52,7 +52,6 @@
 #include <sys/resource.h>
 #include "starboard/common/thread.h"
 #include "starboard/common/media.h"
-#include "starboard/thread.h"
 #include "starboard/common/time.h"
 #include "starboard/drm.h"
 #include "starboard/common/log.h"
